db76f2c84b3483dae6529002b9bef22bf3fd8c92
Natalia Sereda <seredanatasha@gmail.com>
2024-09-16 15:21:43 +0400

    add endpoints: update, delete, create

--- index.js
+++ index.js
@@ -4,19 +4,13 @@ const { filmsRouter } = require('./routers/films');
 const app = express();
 const PORT = 3000;
 
-const handlers = {
-    // '/api/films/readall' : readAllFilms,
-    // '/api/films/read' : readFilmById,
-    // '/api/films/create' : addNewFilm,
-    // '/api/films/update' : updateFilmById,
-    // '/api/films/delete' : deleteFilmById
-}
 app.use(express.json());
 app.get('', (req, res) => {
     res.send('Hello World!!!');
 })
 app.use('/getTop250', filmsRouter);
 app.use('/api', filmsRouter);
+
 app.listen(PORT, () => {
     console.log('Example app listening on port 3000!');
 })
--- routers/films.js
+++ routers/films.js
@@ -1,42 +1,102 @@
 const axios = require("axios");
 const express = require('express');
 const filmsRouter = express.Router();
-const path = require("path");
 const fs = require("fs");
+const fileToPath = './files/top250.json';
 
 let movies = [];
 const url = 'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=250&selectFields=id&selectFields=name&selectFields=year&selectFields=rating&selectFields=budget&selectFields=poster&notNullFields=id&sortField=id&sortField=name&sortType=1&sortType=-1&type=movie&token=0VQXHBN-5JMMWFP-GWRJC8N-Z7JKC3P';
 
+function writeFile(path, arr) {
+    fs.writeFile(path, JSON.stringify(arr, null, 4),(err) => {
+        if (err) {
+            console.error('Error of writing movies file:', err);
+        } else {
+            console.log('File movies successfully created');
+        }
+    });
+}
 filmsRouter.get('/', async(req, res) => {
     try {
         const response = await axios.get(url);
-        const films = response.data.docs;
-        movies = films;
+        const responseFilms = response.data.docs;
 
-        const fileToPath = path.join(__dirname, './files/top250.json');
-        fs.writeFile(fileToPath, JSON.stringify(films, null, 4), () => {});
-        res.status(200).send('Movies are downloaded!')
+        let i = 0
+        movies = responseFilms.map( item => {
+            i += 1
+            return {
+                id: i,
+                title: item.name,
+                rating: item.rating.imdb.toString(),
+                year: item.year,
+                budget: item.budget?.value ?? 0,
+                gross: "Undefiend",
+                poster: item.poster.url,
+                position: item.id
+            }
+        })
+        writeFile(fileToPath, movies);
+        res.status(200).send(`Movies are downloaded and converted! ${movies.length}`)
     } catch (err) {
         console.error(err);
         res.status(500).send(err);
     }
 })
 filmsRouter.get('/films/readall',(req, res) => {
-    const sortedMovies = movies.sort((a, b) => a.id - b.id);
-    res.status(200).send(sortedMovies);
+    const sortedMoviesByPosition = [...movies].sort((a, b) => a.position - b.position);
+    res.status(200).send(sortedMoviesByPosition);
 })
 filmsRouter.get('/films/read', (req, res) =>{
     const { id } = req.query;
-    console.log(id);
-    let filmById = movies.filter(item => item.id === Number(id));
-    console.log(filmById);
+    let filmById = movies.find(item => item.id === Number(id));
     if (filmById) {
         return res.status(200).send(filmById);
     } else {
         return res.status(404).send('Film not found');
     }
 })
-
+filmsRouter.post('/films/update', (req, res) => {
+    const { id } = req.body;
+    let filmById = movies.find(item => item.id === Number(id));
+    let filmIndex = movies.findIndex(item => item.id === Number(id));
+    if (!filmById) {
+        return res.status(404).send('Film not found');
+    } else {
+        let newBody = req.body;
+        for (let key in newBody) {
+            filmById[key] = newBody[key];
+        }
+        movies[filmIndex] = filmById;
+        writeFile(fileToPath, movies);
+        return res.status(200).send(filmById);
+    }
+})
+filmsRouter.post('/films/delete', (req, res) => {
+    const { id } = req.query;
+    if (id > 0 && id < movies.length) {
+        movies.splice(id - 1, 1);
+        writeFile(fileToPath, movies);
+        return res.status(200).send('Delete successfully');
+    } else {
+        return res.status(404).send('Film not found');
+    }
+})
+filmsRouter.post('/films/create', (req, res) => {
+    const { body } = req;
+    const newFilm = {
+        id : movies.length + 1,
+        title : body.title,
+        rating : body.rating,
+        year : body.year,
+        budget : body.budget,
+        poster : body.poster,
+        position : body.position
+    }
+    movies.push(newFilm);
+    writeFile(fileToPath, movies);
+    // fs.writeFile(fileToPath, JSON.stringify(movies, null, 4), () => {});
+    res.status(200).send(`Added new movie: ${newFilm}`);
+})
 module.exports = {
     filmsRouter
 }
